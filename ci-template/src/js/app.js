9b4516386e30932fd8d277aac4079360d985887d
Dmitry Pigrov <pigrov@gmail.com>
2022-06-23 22:07:43 +0300

    jest

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,7 +1,13 @@
-// TODO: write your code here
-import sum from './basic';
-import health from './components/health';
+import Bowman from './components/Bowman';
+import Swordsman from './components/Swordsman';
+import Magician from './components/Magician';
+import Daemon from './components/Daemon';
+import Undead from './components/Undead';
+import Zombie from './components/Zombie';
 
-console.log('worked');
 
-console.log(sum([1, 2]));
+const deamon = new Daemon({
+  name: 'Crouly',
+});
+
+console.log(deamon);
